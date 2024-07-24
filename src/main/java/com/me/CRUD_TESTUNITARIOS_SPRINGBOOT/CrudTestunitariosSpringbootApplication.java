626ef3917ea0be24ecc2ec6eec18925d701ab286
nilson <nilsonjmn21@gmail.com>
2024-07-24 14:37:00 -0500

    -

---
+++ src/main/java/com/me/CRUD_TESTUNITARIOS_SPRINGBOOT/CrudTestunitariosSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.me.CRUD_TESTUNITARIOS_SPRINGBOOT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudTestunitariosSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudTestunitariosSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/me/CRUD_TESTUNITARIOS_SPRINGBOOT/CrudTestunitariosSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.me.CRUD_TESTUNITARIOS_SPRINGBOOT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudTestunitariosSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
